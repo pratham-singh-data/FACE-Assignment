355ba8677d49d7bd1ed58b75321bccabb0760a74
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-08-01 21:58:46 +0530

    Main Page Done

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,10 +1,8 @@
-import { Api } from "@mui/icons-material"
-
 export const getData = async () => {
     // fetch data from random user Api, convert it to JSON then return only results from JSON object hence recieved, errors if any are displayed to the user as alerts
     const data = await fetch("https://randomuser.me/api/?results=5000").then(res => res.json()).then(res => res.results).catch(err => {
         alert(err.message)
     });
-    
+
     return data;
 }
